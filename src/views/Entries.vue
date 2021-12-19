5686652d0095b1dc5811467922ecf49a892936e9
Michel-Lukas <stubbe@Lukass-MacBook-Air.fritz.box>
2021-12-19 16:15:31 +0100

    Backend Connection established

--- src/views/Entries.vue
+++ src/views/Entries.vue
@@ -2,7 +2,7 @@
   <h1>This is where you find all your Diary Entries!</h1>
   <ul class="list-group list-group-flush">
     <div class="col" v-for="entry in entries" :key="entry.id">
-      <li class="list-group-item">Entry from {{entry.date}}. Written at {{entry.time}} </li>
+      <li class="list-group-item">Entry from {{entry.date}}. {{ entry.input }} </li>
     </div>
   </ul>
   <button style="position:absolute;bottom:20px;right:25px;margin:0;padding:5px 5px;" type="button" class="btn btn-primary">Write New Entry</button>
@@ -17,20 +17,18 @@ export default {
       entries: []
     }
   },
-  methods: {
-    mounted () {
-      const requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-      }
-
-      fetch('http://localhost:8080/api/v1/entries', requestOptions)
-        .then(response => response.json())
-        .then(result => result.forEach(entry => {
-          this.entries.push(entry)
-        }))
-        .catch(error => console.log('error', error))
+  mounted () {
+    const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/entries'
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
     }
+    fetch(endpoint, requestOptions)
+      .then(response => response.json())
+      .then(result => result.forEach(entry => {
+        this.entries.push(entry)
+      }))
+      .catch(error => console.log('error', error))
   }
 }
 </script>
