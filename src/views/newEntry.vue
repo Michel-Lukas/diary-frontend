65ce290b9362dacf85f23fccef1067b9e58ea5ee
Michel-Lukas <stubbe@Lukass-Air.fritz.box>
2022-01-03 19:18:05 +0100

    Frontend Creation working

--- src/views/newEntry.vue
+++ src/views/newEntry.vue
@@ -16,7 +16,7 @@
       <div class="col">
         <div class="form-group">
           <label></label>
-          <textarea class="form-control" rows="5" id="comment"></textarea>
+          <textarea class="form-control" rows="5" id="comment" v-model="input"></textarea>
           <small class="form-text text-muted">
             This is a safe space to write about your day.
           </small>
@@ -25,7 +25,7 @@
     </div>
     <div class="row">
       <div class="col">
-        <button type="button" class="btn btn-outline-primary">Done!</button>
+        <button class="btn btn-outline-primary" type="submit" @click.prevent="createEntry">Done!</button>
       </div>
     </div>
   </div>
@@ -33,7 +33,42 @@
 
 <script>
 export default {
-  name: 'newEntry'
+  name: 'newEntry',
+  data () {
+    return {
+      input: '',
+      date: '',
+      time: ''
+    }
+  },
+  methods: {
+    createEntry () {
+      const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/entries'
+      const today = new Date().toISOString().slice(0, 10)
+      const timehelp = new Date()
+      const time = timehelp.getHours() + ':' + timehelp.getMinutes()
+      const myHeaders = new Headers()
+      myHeaders.append('Content-Type', 'application/json')
+
+      const raw = JSON.stringify({
+        input: this.input,
+        date: today,
+        time: time
+      })
+
+      const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      }
+      console.log(time)
+
+      fetch(endpoint, requestOptions)
+        .catch(error => console.log('error', error))
+      console.log(this.input)
+    }
+  }
 }
 </script>
 
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   devServer: {
-    port: '3'
+    port: '1024'
   }
 }
