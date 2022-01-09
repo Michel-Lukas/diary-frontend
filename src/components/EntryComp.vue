a014825133110de73afaef4ed0cec4aaf50bfde3
Michel-Lukas <stubbe@Lukass-Air.fritz.box>
2022-01-09 19:16:52 +0100

    Testing new Entry View

---
+++ src/components/EntryComp.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "EntryComp"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/EntryCompList.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "EntryCompList"
+}
+</script>
+
+<style scoped>
+
+</style>
