7cfc0d3bf9a0c19478f12ba5ec02f4a0a5dbc48f
Michel-Lukas <stubbe@Lukass-MacBook-Air.fritz.box>
2022-01-09 19:50:01 +0100

    entry card

--- src/views/Entries.vue
+++ src/views/Entries.vue
@@ -1,17 +1,25 @@
 <template>
   <h1>This is where you find all your Diary Entries!</h1>
   <div class="container-fluid">
-    <entry-comp-list :entries="this.entries"></entry-comp-list>
+    <div class="row row-cols-1 row-cols-md-4 g-4">
+      <div class="col" v-for="entry in entries" :key="entry.id">
+        <div class="card text-white bg-primary mb-3">
+          <div class="card-header">Header</div>
+          <div class="card-body">
+            <h5 class="card-title">{{entry.date}} at {{entry.time}}</h5>
+            <p class="card-text">{{entry.input}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
   <button style="position:absolute;bottom:20px;right:25px;margin:0;padding:5px 5px;" type="button" class="btn btn-primary">Write New Entry</button>
 </template>
 
 <script>
 
-import EntryCompList from '../components/EntryCompList'
 export default {
   name: 'Entries',
-  components: { EntryCompList },
   data () {
     return {
       entries: []
